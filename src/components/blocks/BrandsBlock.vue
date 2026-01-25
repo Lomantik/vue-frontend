b6e31d0ac12da7e62ede1326b93e7cf03b9bfd4d
Lomantik <lomantikus@gmail.com>
2026-01-25 22:56:59 +0300

    Added responsiveness fixes

--- src/components/blocks/BrandsBlock.vue
+++ src/components/blocks/BrandsBlock.vue
@@ -4,8 +4,8 @@
 
 <template>
   <div class="brands container">
-    <div class="px-md-90">
-      <div class="row g-40 g-md-60 text-center">
+    <div class="px-0 px-md-90">
+      <div class="row g-40 g-md-60 text-center w-100">
         <div class="col-md">
           <img class="w-md-100 mw-180 mw-md-100 h-auto" src="/l-1.png" alt="" width="360" height="180">
         </div>
