5e9af66c05480127f0461c667549cd76a2ba3117
Lomantik <lomantikus@gmail.com>
2026-01-26 20:11:18 +0300

    Image urls fix added

--- src/composables/useResponsiveImage.js
+++ src/composables/useResponsiveImage.js
@@ -1,4 +1,4 @@
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { getImageById, getImageBySlug } from '@/api/images.api.js'
 
 /** @typedef {import('@/types/images.js').Image} Image */
@@ -6,7 +6,17 @@ import { getImageById, getImageBySlug } from '@/api/images.api.js'
 export function useResponsiveImage(imageKey) {
   /** @type {Image} */
   const image = ref()
-  const srcset = ref('')
+  const resolvedSrc = computed(() => {
+    if (!image.value) return ''
+
+    return import.meta.env.BASE_URL + image.value.src
+  })
+  const resolvedSrcset = computed(() => {
+    if (!image.value?.srcset?.length) return ''
+
+    return image.value.srcset.map(i => `${import.meta.env.BASE_URL}${i.src} ${i.w}w`).join(', ')
+      + `, ${resolvedSrc.value} ${image.value.width}w`
+  })
 
   watch(
     () => imageKey.value,
@@ -18,15 +28,12 @@ export function useResponsiveImage(imageKey) {
       } else {
         image.value = await getImageBySlug(imageKey)
       }
-      if (image.value?.srcset?.length > 0) {
-        srcset.value = image.value.srcset.map(i => `${import.meta.env.BASE_URL}${i.src} ${i.w}w`)
-          .join(', ') + `, ${import.meta.env.BASE_URL}${image.value.src} ${image.value.width}w`
-      }
     }, { immediate: true }
   )
 
   return {
     image,
-    srcset
+    src: resolvedSrc,
+    srcset: resolvedSrcset
   }
 }
